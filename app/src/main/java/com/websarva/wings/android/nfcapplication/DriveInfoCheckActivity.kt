4c67cecabb96104e5f872d4ee27d7cc2c76eaf12
hswuser <a>
2025-01-24 16:19:34 +0900

    本人確認画面のレイアウト変更

--- app/src/main/java/com/websarva/wings/android/nfcapplication/DriveInfoCheckActivity.kt
+++ app/src/main/java/com/websarva/wings/android/nfcapplication/DriveInfoCheckActivity.kt
@@ -1,5 +1,6 @@
 package com.websarva.wings.android.nfcapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.widget.TextView
 import com.gemalto.jp2.JP2Decoder
 
 class DriveInfoCheckActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drive_info_check)
@@ -17,8 +19,8 @@ class DriveInfoCheckActivity : AppCompatActivity() {
         val faceImage = findViewById<ImageView>(R.id.face)
         val buttonTop = findViewById<Button>(R.id.buttonTop)
 
-        name.text = intent.getStringExtra("name")
-        birth.text = intent.getStringExtra("birth")
+        name.text = "名前：${intent.getStringExtra("name")}"
+        birth.text = "生年月日：${intent.getStringExtra("birth")}"
         val face: ByteArray? = intent.getByteArrayExtra("face")
 
         faceImage.setImageBitmap(JP2Decoder(face).decode())
